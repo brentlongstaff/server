5b85bc16ca7bed224948ede3732b0d3dcd75819c
BL <brent@fake.email>
2014-05-08 03:37:47 -0700

    d

--- src/org/ohmage/routinesense/RoutineSense.java
+++ src/org/ohmage/routinesense/RoutineSense.java
@@ -14,7 +14,7 @@ import org.joda.time.DateTime;
 public class RoutineSense {
 	/*HashMap<String, ArrayList<String>>*/ public static String getDayEvents(String username, DateTime date)
 	{
-		ProcessBuilder builder = new ProcessBuilder("mono", "/home/blongsta/RoutineSense/Loris.exe", username, date.year() + "-" + date.monthOfYear() + "-" + date.dayOfMonth());
+		ProcessBuilder builder = new ProcessBuilder("mono", "/home/blongsta/RoutineSense/Loris.exe", username, date.getYear() + "-" + date.getMonthOfYear() + "-" + date.getDayOfMonth());
 //		ProcessBuilder builder = new ProcessBuilder("echo", "{ \"frog\" : 3 }");
 		
 		try {
